f42b636e7c416ed97106e7b34162c2015b5a3178
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2021-12-15 14:16:23 -0500

    adding mqtt explanation

--- src/pages/components/PageWrapper.tsx
+++ src/pages/components/PageWrapper.tsx
@@ -1,8 +1,23 @@
+import Head from 'next/head'
+
 export const PageWrapper: React.FC = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>suunn</title>
+        <link
+          rel="icon"
+          href="https://emojiguide.org/images/emoji/q/1qmup5ljp0kmq.png"
+        />
+      </Head>
       <div className="h-full pb-20 bg-gradient-to-t from-yellow-100">
         {children}
+        <footer className="flex justify-center mx-auto mt-20">
+          <p>
+            Powered by <a href="https://benji.ar">Ben 🧑🏼‍💻 </a>and{' '}
+            <a href="https://github.com/daninewbs">Danielle 👩🏻‍💻</a>
+          </p>
+        </footer>
       </div>
     </>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import { SketchPicker } from 'react-color'
@@ -17,16 +16,16 @@ const Home: NextPage = () => {
     }
     fetch(API_URL, postReqOpts)
   }, [color])
+
+  const [showDetail, setShowDetail] = useState<boolean>(false)
   return (
     <PageWrapper>
-      <Head>
-        <title>suunn</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center h-screen max-w-3xl mx-auto">
-        <h1 className="my-10 font-mono text-6xl font-bold text-yellow-600">
+      <main className="flex flex-col items-center h-screen max-w-3xl mx-auto">
+        <h1 className="mt-10 font-mono text-6xl font-bold text-yellow-600">
           suunns
+        </h1>
+        <h1 className="mb-10 font-mono text-6xl font-bold text-yellow-100">
+          snnuus
         </h1>
         <SketchPicker
           color={color}
@@ -34,14 +33,30 @@ const Home: NextPage = () => {
             setColor(color.hex)
           }
         />
-        <div className="px-4 py-2 mt-4 text-white bg-yellow-500 rounded-md cursor-pointer hover:bg-blue-900">
-          <Link href="/report">Report</Link>
+        <button
+          onClick={() => setShowDetail((v) => !v)}
+          className="px-4 py-2 mt-4 font-bold text-black rounded-full hover:bg-gray-400 hover:text-white cursor-help"
+        >
+          What does this do?
+        </button>
+        {showDetail && (
+          <p className="text-center">
+            When you click on the color component above, we publish the color to
+            our MQTT broker under the <code>suunn/color</code> topic. This means
+            that any devices subscribed to that topic will get the message. Ours
+            suunns are listening for messages on this topic to change their
+            light color.
+          </p>
+        )}
+        <div className="flex justify-around w-5/12 pt-8">
+          <div className="px-4 py-2 mt-4 text-white bg-yellow-500 rounded-md cursor-pointer hover:bg-blue-900">
+            <Link href="/report">Final Writeup</Link>
+          </div>
+          <div className="px-4 py-2 mt-4 text-white bg-black rounded-md cursor-pointer hover:bg-gray-900">
+            <Link href="/report">Github Repo</Link>
+          </div>
         </div>
       </main>
-
-      <footer className="flex justify-center mx-auto mt-20">
-        <p>Powered by Ben and Danielle</p>
-      </footer>
     </PageWrapper>
   )
 }
